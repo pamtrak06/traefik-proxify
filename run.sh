6bc26f0387915accb6438c852abf9ad0f76694b3
pamtrak06 <pamtrak06@gmail.com>
2020-02-28 16:50:21 +0100

    port 443

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 docker rm --force traefik-wms
 #docker run -d -p 8080:8080 -p 80:80 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v $PWD/traefik-dynamic.toml:/etc/traefik/traefik-dynamic.toml --name traefik-wms traefik:v2.1.4
-docker run -d -p 8080:8080 -p 80:80 --name traefik-wms traefik:v2.0-alpine
+docker run -d -p 8080:8080 -p 80:80 -p 443:443 --name traefik-wms traefik:v2.0-alpine
 docker exec -t traefik-wms mkdir /etc/traefik
 docker cp traefik.toml traefik-wms:/etc/traefik/
 docker cp traefik-dynamic.toml traefik-wms:/etc/traefik/
