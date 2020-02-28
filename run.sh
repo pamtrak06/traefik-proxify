9de8a2aba49c1715c541a02cf9d3c13511545817
pamtrak06 <pamtrak06@gmail.com>
2020-02-28 11:25:56 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 docker rm --force traefik-wms
 #docker run -d -p 8080:8080 -p 80:80 -v $PWD/traefik.toml:/etc/traefik/traefik.toml -v $PWD/traefik-dynamic.toml:/etc/traefik/traefik-dynamic.toml --name traefik-wms traefik:v2.1.4
 docker run -d -p 8080:8080 -p 80:80 --name traefik-wms traefik:v2.0-alpine
+docker exec -t traefik-wms mkdir /etc/traefik
 docker cp traefik.toml traefik-wms:/etc/traefik/
 docker cp traefik-dynamic.toml traefik-wms:/etc/traefik/
 docker restart traefik-wms && docker logs -f traefik-wms
